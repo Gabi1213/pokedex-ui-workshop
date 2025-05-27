f2fc5fad7de50784c92e87763b36ad9b420d5537
Andre <gabi_andrei_01@yahoo.com>
2025-05-27 15:19:52 +0300

    3d fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@ const nextConfig = {
        return [
            {
                source: '/api/pokemon',
-               destination: 'https://pkdqe17fh2.execute-api.eu-west-2.amazonaws.com/Prod/Prod/pokemon',
+               destination: 'https://pkdqe17fh2.execute-api.eu-west-2.amazonaws.com/Prod/pokemon',
            },
            {
                source: '/api/pokemon/:id',
-               destination: 'https://pkdqe17fh2.execute-api.eu-west-2.amazonaws.com/Prod/Prod/pokemon/:id',
+               destination: 'https://pkdqe17fh2.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/:id',
            },
        ];
    }
